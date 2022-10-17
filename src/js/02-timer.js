b27c8e8dad804af0be011d9156f9f91027926918
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-10-18 00:05:35 +0300

    fix task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,8 +5,9 @@ import 'flatpickr/dist/themes/material_blue.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import FlipDown from 'flipdown-mp';
+import '../css/timer.css';
 
-// flatpickr('#datetime-picker');
 const calendar = flatpickr('#datetime-picker', {
   locale: Russian,
   enableTime: true,
@@ -20,48 +21,77 @@ const calendar = flatpickr('#datetime-picker', {
 
 const refs = {
   startTimerBtn: document.querySelector('[data-start]'),
+  stopTimerBtn: document.querySelector('[data-stop]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
 
-function dateValidate(InputDate) {
-  if (new Date() >= InputDate) {
+function dateValidate(inputDate) {
+  if (new Date() >= inputDate) {
     refs.startTimerBtn.disabled = 'true';
     return Notiflix.Notify.warning('Please choose a date in the future');
   }
   refs.startTimerBtn.removeAttribute('disabled');
-  updateClockFace(convertMs(InputDate.getTime()));
+  Notiflix.Notify.success('Press start for countdown');
 }
 
-refs.startTimerBtn.addEventListener('click', timer);
+let intervalId = null;
+const INTERVAL_TIME = 1000;
+refs.startTimerBtn.addEventListener('click', startTimer);
+refs.stopTimerBtn.addEventListener('click', stopTimer);
 
-function timer() {
-  Notiflix.Notify.success('Start counting time');
-  const startTime = calendar.selectedDates[0].getTime();
+function startTimer() {
+  refs.startTimerBtn.disabled = 'true';
   const dateNow = Date.now();
-  setInterval(() => {
-    const deltaTime = dateNow - Date.now();
-    updateClockFace(convertMs(startTime + deltaTime));
-  }, 1000);
+  const userTime = calendar.selectedDates[0].getTime() - Date.now();
+  new FlipDown(
+    Math.floor(
+      new Date(calendar.selectedDates[0] - INTERVAL_TIME).getTime() / 1000
+    )
+  ).start();
+
+  intervalId = setInterval(() => {
+    deltaTime = Date.now() - dateNow;
+    if (userTime < deltaTime + INTERVAL_TIME) {
+      clearInterval(intervalId);
+      return;
+    }
+    updateClockFace(convertMs(userTime - deltaTime));
+  }, INTERVAL_TIME);
 }
 
-function convertMs(time) {
-  const days = pad(Math.floor((time / (1000 * 60 * 60 * 24)) % 30));
-  const hours = pad(Math.floor((time / (1000 * 60 * 60)) % 24));
-  const mins = pad(Math.floor((time / (1000 * 60)) % 60));
-  const secs = pad(Math.floor((time / 1000) % 60));
-
-  return { days, hours, mins, secs };
+function stopTimer() {
+  location.reload();
 }
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
 function pad(value) {
   return String(value).padStart(2, '0');
 }
 
-function updateClockFace({ days, hours, mins, secs }) {
+function updateClockFace({ days, hours, minutes, seconds }) {
   refs.days.textContent = days;
   refs.hours.textContent = hours;
-  refs.minutes.textContent = mins;
-  refs.seconds.textContent = secs;
+  refs.minutes.textContent = minutes;
+  refs.seconds.textContent = seconds;
 }
--- src/js/temp.js
+++ src/js/temp.js
@@ -1,3 +1,95 @@
+// import flatpickr from 'flatpickr';
+// import { Russian } from 'flatpickr/dist/l10n/ru.js';
+// import 'flatpickr/dist/themes/material_blue.css';
+
+// import 'flatpickr/dist/flatpickr.min.css';
+// import Notiflix from 'notiflix';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// // flatpickr('#datetime-picker');
+// flatpickr('#datetime-picker', {
+//   locale: Russian,
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     dateValidate(selectedDates[0]);
+//   },
+// });
+
+// const refs = {
+//   startTimerBtn: document.querySelector('[data-start]'),
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+// };
+
+// function dateValidate(InputDate) {
+//   if (new Date() >= InputDate) {
+//     refs.startTimerBtn.disabled = 'true';
+//     return Notiflix.Notify.warning('Please choose a date in the future');
+//   }
+//   console.log('YES');
+//   refs.startTimerBtn.removeAttribute('disabled');
+// }
+
+// class Timer {
+//   constructor({ onTic }) {
+//     this.intervalId = null;
+//     this.isActive = false;
+//     this.onTic = onTic;
+
+//     this.init();
+//   }
+//   init() {
+//     const time = this.convertMs(0);
+//     this.onTic(time);
+//   }
+//   start() {
+//     if (this.isActive) {
+//       return;
+//     }
+//     const startTime = Date.now();
+//     this.isActive = true;
+
+//     this.intervalId = setInterval(() => {
+//       const currentTime = Date.now();
+//       const deltaTime = currentTime - startTime;
+//       const time = this.convertMs(deltaTime);
+//       this.onTic(time);
+//     }, 1000);
+//   }
+//   stop() {
+//     const time = this.convertMs(0);
+//     this.onTic(time);
+//     this.isActive = false;
+//     clearInterval(this.intervalId);
+//   }
+
+//   convertMs(time) {
+//     const days = this.pad(Math.floor((time / (1000 * 60 * 60 * 24)) % 30));
+//     const hours = this.pad(Math.floor((time / (1000 * 60 * 60)) % 24));
+//     const mins = this.pad(Math.floor((time / (1000 * 60)) % 60));
+//     const secs = this.pad(Math.floor((time / 1000) % 60));
+
+//     return { days, hours, mins, secs };
+//   }
+//   pad(value) {
+//     return String(value).padStart(2, '0');
+//   }
+// }
+// const timer = new Timer({ onTic: updateClockFace });
+// refs.startTimerBtn.addEventListener('click', timer.start.bind(timer));
+
+// function updateClockFace({ days, hours, mins, secs }) {
+//   refs.days.textContent = days;
+//   refs.hours.textContent = hours;
+//   refs.minutes.textContent = mins;
+//   refs.seconds.textContent = secs;
+// }
+
 import flatpickr from 'flatpickr';
 import { Russian } from 'flatpickr/dist/l10n/ru.js';
 import 'flatpickr/dist/themes/material_blue.css';
@@ -6,8 +98,9 @@ import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// flatpickr('#datetime-picker');
-flatpickr('#datetime-picker', {
+import '../css/timer.css';
+
+const calendar = flatpickr('#datetime-picker', {
   locale: Russian,
   enableTime: true,
   time_24hr: true,
@@ -26,62 +119,46 @@ const refs = {
   seconds: document.querySelector('[data-seconds]'),
 };
 
-function dateValidate(InputDate) {
-  if (new Date() >= InputDate) {
+function dateValidate(inputDate) {
+  if (new Date() >= inputDate) {
     refs.startTimerBtn.disabled = 'true';
     return Notiflix.Notify.warning('Please choose a date in the future');
   }
-  console.log('YES');
   refs.startTimerBtn.removeAttribute('disabled');
+  Notiflix.Notify.success('Press start counting time');
 }
 
-class Timer {
-  constructor({ onTic }) {
-    this.intervalId = null;
-    this.isActive = false;
-    this.onTic = onTic;
+let intervalId = null;
+const INTERVAL_TIME = 1000;
+refs.startTimerBtn.addEventListener('click', timer);
 
-    this.init();
-  }
-  init() {
-    const time = this.convertMs(0);
-    this.onTic(time);
-  }
-  start() {
-    if (this.isActive) {
+function timer() {
+  const dateNow = Date.now();
+  const userTime = calendar.selectedDates[0].getTime() - Date.now();
+
+  intervalId = setInterval(() => {
+    deltaTime = Date.now() - dateNow;
+    if (userTime < deltaTime + INTERVAL_TIME) {
+      clearInterval(intervalId);
       return;
     }
-    const startTime = Date.now();
-    this.isActive = true;
 
-    this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const time = this.convertMs(deltaTime);
-      this.onTic(time);
-    }, 1000);
-  }
-  stop() {
-    const time = this.convertMs(0);
-    this.onTic(time);
-    this.isActive = false;
-    clearInterval(this.intervalId);
-  }
-
-  convertMs(time) {
-    const days = this.pad(Math.floor((time / (1000 * 60 * 60 * 24)) % 30));
-    const hours = this.pad(Math.floor((time / (1000 * 60 * 60)) % 24));
-    const mins = this.pad(Math.floor((time / (1000 * 60)) % 60));
-    const secs = this.pad(Math.floor((time / 1000) % 60));
-
-    return { days, hours, mins, secs };
-  }
-  pad(value) {
-    return String(value).padStart(2, '0');
-  }
+    updateClockFace(convertMs(userTime - deltaTime));
+  }, INTERVAL_TIME);
 }
-const timer = new Timer({ onTic: updateClockFace });
-refs.startTimerBtn.addEventListener('click', timer.start.bind(timer));
+
+function convertMs(time) {
+  const days = pad(Math.floor((time / (1000 * 60 * 60 * 24)) % 30));
+  const hours = pad(Math.floor((time / (1000 * 60 * 60)) % 24));
+  const mins = pad(Math.floor((time / (1000 * 60)) % 60));
+  const secs = pad(Math.floor((time / 1000) % 60));
+
+  return { days, hours, mins, secs };
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
 function updateClockFace({ days, hours, mins, secs }) {
   refs.days.textContent = days;
